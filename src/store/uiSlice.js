4e56dc3563bf3c7a440db7b681a8ede583f11554
Sumit0397 <sumitmajumdar16@gmail.com>
2023-09-09 20:50:57 +0530

    Doing Network calls and andling response the right way

--- src/store/uiSlice.js
+++ src/store/uiSlice.js
@@ -1,15 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {cartisVisible : false}
+const initialState = { cartisVisible: false, notification: null }
 
 const uiSlice = createSlice({
     name: "ui",
     initialState,
-    reducers : {
+    reducers: {
         toggle(state) {
             state.cartisVisible = !state.cartisVisible
+        },
+        showNotification(state, action) {
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message,
+            };
         }
-    } 
+    }
 })
 
 export const uiActions = uiSlice.actions;
