421cbf3a31a5de4bf271cf991ccbdaaded32152e
Sumit0397 <sumitmajumdar16@gmail.com>
2023-09-09 12:34:08 +0530

    Shoping page app using reducers

---
+++ src/store/uiSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {cartisVisible : false}
+
+const uiSlice = createSlice({
+    name: "ui",
+    initialState,
+    reducers : {
+        toggle(state) {
+            state.cartisVisible = !state.cartisVisible
+        }
+    } 
+})
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice.reducer;
+
+
